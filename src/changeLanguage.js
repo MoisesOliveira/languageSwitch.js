8629f1a7c68c99de93f7be7361649217708fa479
Moisés Santos Oliveira <moises.santos.oliveira91@gmail.com>
2022-01-17 11:03:38 -0300

    changeLanguage function added

---
+++ src/changeLanguage.js
@@ -0,0 +1,17 @@
+/**
+ * @param specifiedLanguage - Specify which index in languages array you desire.
+ * @param languagesArray - Array of languages objects with it properties
+ */
+let changeLanguage = (specifiedLanguage, languagesArray) =>{
+
+    for(properties in languages[specifiedLanguage]){
+        let elements = document.getElementsByClassName(`${properties}`)
+
+        for(let i = 0; i<elements.length; i++){
+            elements[i].innerHTML = languagesArray[specifiedLanguage][properties]
+        }
+    }
+}
+
+module.exports = changeLanguage;
+
